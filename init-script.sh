18bd37af56d426b49be087bbb70a5e2c4cd2faa5
Ilya Kohanovskiy <i.kohanvs@gmail.com>
2020-11-27 16:28:49 +0300

    add .gitignore

--- init-script.sh
+++ init-script.sh
@@ -1,16 +1,6 @@
-#!/bin/bash
-yum update -y
-yum -y remove httpd
-yum -y remove httpd-tools
-yum install -y httpd24 php72 mysql57-server php72-mysqlnd
-service httpd start
-chkconfig httpd on
-
-usermod -a -G apache ec2-user
-chown -R ec2-user:apache /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} \;
-find /var/www -type f -exec chmod 0664 {} \;
-cd /var/www/html
-curl http://169.254.169.254/latest/meta-data/instance-id -o index.html
-curl https://raw.githubusercontent.com/hashicorp/learn-terramino/master/index.php -O
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Azure Linux VM with Web Server</h1>" | sudo tee /var/www/html/index.html
--- main.tf
+++ main.tf
@@ -1,26 +1,81 @@
-
-provider "aws" {
-  region = "us-west-2"
+provider "azurerm" {
+  features {}
 }
 
-provider "random" {}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
 
-resource "random_pet" "name" {}
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
-resource "aws_instance" "web" {
-  ami           = "ami-a0cfeed8"
-  instance_type = "t2.micro"
-  user_data     = file("init-script.sh")
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-  tags = {
-    Name = random_pet.name.id
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
-output "domain-name" {
-  value = aws_instance.web.public_dns
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  custom_data         = base64encode(data.template_file.linux-vm-cloud-init.rendered)
+
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = var.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
 }
 
-output "application-url" {
-  value = "${aws_instance.web.public_dns}/index.php"
+output "public-id" {
+  value = azurerm_linux_virtual_machine.example.public_ip_address
+}
+
+data "template_file" "linux-vm-cloud-init" {
+  template = file("init-script.sh")
 }
